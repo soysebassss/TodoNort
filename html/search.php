7ddfdd6d8af75ca6a983e03cc543b768e35fa8bb
soysebassss <40040414+soysebassss@users.noreply.github.com>
2018-07-09 14:59:46 -0400

    Template

---
+++ html/search.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Search Results</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/bootstrap-responsive.css" type="text/css" media="screen">    
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+
+<script type="text/javascript" src="js/jquery.js"></script>  
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/superfish.js"></script>
+
+<script type="text/javascript" src="js/jquery.ui.totop.js"></script>
+<script type="text/javascript" src="search/search.js"></script>
+<script>
+$(document).ready(function() {
+	// 
+
+
+}); //
+$(window).load(function() {
+	//
+
+}); //
+</script>		
+<!--[if lt IE 8]>
+		<div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+	<![endif]-->    
+
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>      
+  <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+<![endif]-->
+</head>
+
+<body class="subpage">
+<div id="main">
+
+<header>
+<div class="top1">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="top1_inner clearfix">
+	<div class="logo_wrapper"><a href="index.html" class="logo"><img src="images/logo.png" alt=""></a></div>
+	<div class="search-form-wrapper clearfix">
+		<form id="search-form" action="search.php" method="GET" accept-charset="utf-8" class="navbar-form" >
+			<input type="text" name="s" value='Search' onBlur="if(this.value=='') this.value='Search'" onFocus="if(this.value =='Search' ) this.value=''">
+			<a href="#" onClick="document.getElementById('search-form').submit()"></a>
+		</form>	
+	</div>
+	<div class="social_wrapper">
+		<ul class="social clearfix">
+		    <li><a href="#"><img src="images/social_ic_top1.png"></a></li>
+		    <li><a href="#"><img src="images/social_ic_top2.png"></a></li>		    	    
+		</ul>
+	</div>
+</div>	
+</div>	
+</div>	
+</div>
+</div>	
+<div class="top2">
+<div class="menu_wrapper">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="navbar navbar_">
+	<div class="navbar-inner navbar-inner_">
+		<a class="btn btn-navbar btn-navbar_" data-toggle="collapse" data-target=".nav-collapse_">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</a>
+		<div class="nav-collapse nav-collapse_ collapse">
+			<ul class="nav sf-menu clearfix">
+				<li><a href="index.html">home</a></li>
+				<li><a href="index-1.html">about us</a></li>
+				<li><a href="index-2.html">production services</a></li>
+				<li><a href="index-3.html">events</a></li>
+				<li class="sub-menu sub-menu-1"><a href="index-4.html">photo gallery<em></em></a>
+					<ul>
+						<li><a href="index-4.html">Lorem ipsum dolor</a></li>
+						<li class="sub-menu sub-menu-2"><a href="index-4.html">Sit amet consectetue<em></em></a>
+							<ul>
+								<li><a href="index-4.html">lorem ipsum dolor</a></li>
+								<li><a href="index-4.html">sit amet</a></li>
+								<li><a href="index-4.html">adipiscing elit</a></li>
+								<li><a href="index-4.html">nunc suscipit</a></li>
+								<li><a href="404.html">404 page not found</a></li>
+							</ul>
+						</li>
+						<li class="sub-menu sub-menu-2"><a href="index-4.html">Adipiscing elit<em></em></a>
+							<ul>
+								<li><a href="index-4.html">lorem ipsum dolor</a></li>
+								<li><a href="index-4.html">sit amet</a></li>
+								<li><a href="index-4.html">adipiscing elit</a></li>
+								<li><a href="index-4.html">nunc suscipit</a></li>									
+							</ul>
+						</li>
+						<li><a href="index-4.html">Nunc suscipit</a></li>						
+					</ul>						
+				</li>				
+				<li><a href="index-5.html">resources</a></li>
+				<li><a href="index-6.html">contacts</a></li>											
+    		</ul>
+		</div>
+	</div>
+</div>	
+</div>	
+</div>	
+</div>	
+</div>
+
+</div>
+</header>	
+
+<div id="content1">
+<div class="container">
+<div class="row">		
+	<div class="span12">
+		<h1>Search result:</h1>
+
+		<div id="search-results"></div>
+
+
+
+
+
+	</div>
+</div>	
+</div>
+</div>
+
+<footer>
+<div class="bot1">
+<div class="container">
+<div class="row">
+<div class="span3">
+
+	<div class="block_title">Archives</div>
+
+	<ul class="ul0">
+	  <li><a href="#">May 2013</a></li>
+	  <li><a href="#">April 2013</a></li>
+	  <li><a href="#">March 2013</a></li>
+	  <li><a href="#">February 2013</a></li>
+	  <li><a href="#">January 2013</a></li>	                                                                                
+	</ul>
+
+</div>	
+<div class="span3">
+
+	<div class="block_title">Recent News</div>
+
+	<ul class="ul0">
+	  <li><a href="#">A satisfied customer is the best business strategy of all.</a></li>
+	  <li><a href="#">Good design is good business.</a></li>
+	  <li><a href="#">We design, develop and deliver.</a></li>	                                                                                 
+	</ul>
+
+</div>	
+<div class="span3">
+
+	<div class="block_title">Find us on:</div>
+
+	<ul class="ul_social">
+	  <li><a href="#"><img src="images/social_ic1.jpg" alt="" class="img"><span>Facebook</span></a></li>
+	  <li><a href="#"><img src="images/social_ic2.jpg" alt="" class="img"><span>Twitter</span></a></li>
+	  <li><a href="#"><img src="images/social_ic3.jpg" alt="" class="img"><span>Linked in</span></a></li>
+	  <li><a href="#"><img src="images/social_ic4.jpg" alt="" class="img"><span>Pinterest</span></a></li>
+	</ul>
+
+</div>	
+<div class="span3">
+
+	<div class="block_title">Information</div>
+
+	<p class="txt">
+		<a href="#" class="color2">Lorem ipsum sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</a><br><br>
+		Ipsum dolor sit amet conse ctetur adipisicing elit, sed do <a href="#" class="color1">eiusmod</a> tempor incididunt ut labore et dolore magna aliqua
+	</p>
+
+</div>	
+</div>	
+</div>
+</div>	
+<div class="bot2">
+<div class="container">
+<div class="row">
+<div class="span12">
+	<div class="copyright">Copyright © 2018. GRIDGUM All rights reserved. Production <sup>TM</sup></div>
+</div>	
+</div>	
+</div>	
+</div>
+</footer>
+	
+</div>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
